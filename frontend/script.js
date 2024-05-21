ca8ff0cdb7c1415d0c80dc31f5e32e132f641a11
finnian-smith <finniansmith01@gmail.com>
2024-05-21 13:12:33 +0100

    Add basic code for displaying fetched data

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+const img = document.querySelector("img");
+
+fetch("http://localhost:3000/giphy?s=dogs", { mode: "cors" })
+  .then((response) => response.json)
+  .then((data) => {
+    const imageUrl = data.data.images.original.url;
+
+    img.src = imageUrl;
+  })
+  .catch((error) => {
+    console.error("Error fetching image:", error);
+  });
